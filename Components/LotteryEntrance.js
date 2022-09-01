c096ba50f32c4aadb54bcd7d89164b5092d9c9e5
Kron1749 <drakgr98@gmail.com>
2022-09-01 12:27:53 +0300

    Show minimumValue for Lottery

--- Components/LotteryEntrance.js
+++ Components/LotteryEntrance.js
@@ -1,13 +1,46 @@
 import { useWeb3Contract } from "react-moralis"
+import { abi, contractAddresses } from "../constants"
+import { useMoralis } from "react-moralis"
+import { useEffect, useState } from "react"
 
 export default function LotteryEntrance() {
-    // const {runContractFunction:enterRaffle} = useWeb3Contract(
-    //     abi:
-    //     contractAddress:
-    //     functionName:
-    //     params:{}
-    //     msgValue:
-    // )
+    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
+    const chainId = parseInt(chainIdHex)
+    const lotteryAddress =
+        chainId in contractAddresses ? contractAddresses[chainId][0] : null
 
-    return <div>Lottery entrance</div>
+    const [minimumValue, setMinimumValue] = useState("0")
+    // const {runContractFunction:enterLottery} = useWeb3Contract({
+    //     abi: abi,
+    //     contractAddress: lotteryAddress,
+    //     functionName: "enterLottery",
+    //     params:{},
+    //     msgValue: //here must be entrance fee
+    // })
+
+    const { runContractFunction: getMinimumValue } = useWeb3Contract({
+        abi: abi,
+        contractAddress: lotteryAddress,
+        functionName: "getMinimumValue",
+        params: {},
+    })
+
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            async function updateUI() {
+                const minimumValueFromCall = (
+                    await getMinimumValue()
+                ).toString()
+                setMinimumValue(minimumValueFromCall)
+                console.log(minimumValue)
+            }
+            updateUI()
+        }
+    }, [isWeb3Enabled])
+
+    return (
+        <div>
+            Lottery entrance<div>{minimumValue}</div>
+        </div>
+    )
 }
